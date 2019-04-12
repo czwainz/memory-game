547bc650b8dda20e3b1fcaca4269d7994cd727eb
Chrissy Zwainz <zchrissychrissy@gmail.com>
2019-04-11 21:20:19 -0600

    init

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace memory
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Let's play a game! Try to remember all the numbers that come out!");
+      Console.WriteLine("Pick a number");
+      string userInput = Console.ReadLine();
+
+      int userNum = Int32.Parse(userInput);
+      Console.WriteLine(userNum);
+
+      //make game class
+      //while playing writelines must match random number generator
+      //if false, game exits
+      //if true, continue playing keep adding more numbers
+
+    }
+  }
+}
